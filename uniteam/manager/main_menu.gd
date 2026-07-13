3d3bb2160f4e7362cc84453393cce47deb7e6025
pxlrs <pixelsibal@gmail.com>
2026-07-13 16:31:34 +0800

    added sample main menu

---
+++ uniteam/manager/main_menu.gd
@@ -0,0 +1,16 @@
+# main_menu.gd
+extends Control
+
+# Path to your actual game manager scene
+@export var game_scene: PackedScene = preload("res://manager/game_manager.tscn")
+
+@onready var background = $background
+
+func _ready() -> void:
+	background.play("default")
+	pass
+
+func _input(event: InputEvent) -> void:
+	# Detect click or screen tap
+	if event is InputEventMouseButton and event.pressed:
+		get_tree().change_scene_to_packed(game_scene)
